c23e2a6e70aa9ffbfcae9fafeb67736f58c492d8
Sandro Mani <manisandro@gmail.com>
2019-10-09 16:07:25 +0200

    Add KadasCoordinateInput

---
+++ kadas/gui/kadascoordinateinput.cpp
@@ -0,0 +1,125 @@
+/***************************************************************************
+    kadascoordinateinput.cpp
+    ------------------------
+    copyright            : (C) 2019 by Sandro Mani
+    email                : smani at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include <QComboBox>
+#include <QHBoxLayout>
+#include <QLineEdit>
+#include <QRegExp>
+
+#include <qgis/qgsproject.h>
+
+#include <kadas/core/kadascoordinateformat.h>
+#include <kadas/gui/kadascoordinateinput.h>
+
+KadasCoordinateInput::KadasCoordinateInput( QWidget *parent )
+  : QWidget( parent )
+{
+  setLayout( new QHBoxLayout() );
+  layout()->setMargin( 0 );
+  layout()->setSpacing( 0 );
+
+  mLineEdit = new QLineEdit( this );
+  layout()->addWidget( mLineEdit );
+  connect( mLineEdit, SIGNAL( textEdited( QString ) ), this, SLOT( entryChanged() ) );
+  connect( mLineEdit, SIGNAL( editingFinished() ), this, SLOT( entryEdited() ) );
+
+  mCrsCombo = new QComboBox( this );
+  mCrsCombo->addItem( "LV03" );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, KadasCoordinateFormat::Default, sFormatRole );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, "EPSG:21781", sAuthidRole );
+  mCrsCombo->addItem( "LV95" );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, KadasCoordinateFormat::Default, sFormatRole );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, "EPSG:2056", sAuthidRole );
+  mCrsCombo->addItem( "DMS" );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, KadasCoordinateFormat::DegMinSec, sFormatRole );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, "EPSG:4326", sAuthidRole );
+  mCrsCombo->addItem( "DM" );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, KadasCoordinateFormat::DegMin, sFormatRole );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, "EPSG:4326", sAuthidRole );
+  mCrsCombo->addItem( "DD" );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, KadasCoordinateFormat::Default, sFormatRole );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, "EPSG:4326", sAuthidRole );
+  mCrsCombo->addItem( "UTM" );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, KadasCoordinateFormat::UTM, sFormatRole );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, "EPSG:4326", sAuthidRole );
+  mCrsCombo->addItem( "MGRS" );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, KadasCoordinateFormat::MGRS, sFormatRole );
+  mCrsCombo->setItemData( mCrsCombo->count() - 1, "EPSG:4326", sAuthidRole );
+  mCrsCombo->setCurrentIndex( QgsProject::instance()->readNumEntry( "crsdisplay", "format" ) );
+  layout()->addWidget( mCrsCombo );
+  connect( mCrsCombo, SIGNAL( currentIndexChanged( int ) ), this, SLOT( crsChanged() ) );
+
+  mCrs = QgsCoordinateReferenceSystem( mCrsCombo->currentData( sAuthidRole ).toString() );
+}
+
+void KadasCoordinateInput::setCoordinate( const QgsPoint &coo, const QgsCoordinateReferenceSystem &crs )
+{
+  mEmpty = false;
+  mCoo = QgsCoordinateTransform( crs, mCrs, QgsProject::instance() ).transform( coo );
+  KadasCoordinateFormat::Format format = static_cast<KadasCoordinateFormat::Format>( mCrsCombo->currentData( sFormatRole ).toInt() );
+  QString authId = mCrsCombo->currentData( sAuthidRole ).toString();
+  mLineEdit->setText( KadasCoordinateFormat::instance()->getDisplayString( mCoo, mCrs, format, authId ) );
+  mLineEdit->setStyleSheet( "" );
+  emit coordinateChanged();
+}
+
+void KadasCoordinateInput::entryChanged()
+{
+  mLineEdit->setStyleSheet( "" );
+}
+
+void KadasCoordinateInput::entryEdited()
+{
+  QString text = mLineEdit->text();
+  if ( text.isEmpty() )
+  {
+    mCoo = QgsPoint();
+    mEmpty = true;
+  }
+  else
+  {
+    KadasCoordinateFormat::Format format = static_cast<KadasCoordinateFormat::Format>( mCrsCombo->currentData( sFormatRole ).toInt() );
+    bool valid = false;
+    mCoo = KadasCoordinateFormat::instance()->parseCoordinate( text, format, valid );
+    mEmpty = !valid;
+    if ( !valid )
+    {
+      mLineEdit->setStyleSheet( "background: #FF7777; color: #FFFFFF;" );
+    }
+  }
+  emit coordinateEdited();
+  emit coordinateChanged();
+}
+
+void KadasCoordinateInput::crsChanged()
+{
+  KadasCoordinateFormat::Format format = static_cast<KadasCoordinateFormat::Format>( mCrsCombo->currentData( sFormatRole ).toInt() );
+  QString authId = mCrsCombo->currentData( sAuthidRole ).toString();
+  if ( !mEmpty )
+  {
+    mCoo = QgsCoordinateTransform( mCrs, QgsCoordinateReferenceSystem( authId ), QgsProject::instance() ).transform( mCoo );
+  }
+  mCrs = QgsCoordinateReferenceSystem( authId );
+  if ( !mEmpty )
+  {
+    mLineEdit->setText( KadasCoordinateFormat::instance()->getDisplayString( mCoo, mCrs, format, authId ) );
+  }
+  else
+  {
+    mLineEdit->setText( "" );
+  }
+  mLineEdit->setStyleSheet( "" );
+}
---
+++ kadas/gui/kadascoordinateinput.h
@@ -0,0 +1,64 @@
+/***************************************************************************
+    kadascoordinateinput.h
+    ----------------------
+    copyright            : (C) 2019 by Sandro Mani
+    email                : smani at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KADASCOORDINATEINPUT_H
+#define KADASCOORDINATEINPUT_H
+
+#include <QWidget>
+
+#include <qgis/qgscoordinatereferencesystem.h>
+#include <qgis/qgspoint.h>
+
+#include <kadas/gui/kadas_gui.h>
+
+class QComboBox;
+class QLineEdit;
+
+
+class KADAS_GUI_EXPORT KadasCoordinateInput : public QWidget
+{
+    Q_OBJECT
+
+  public:
+    KadasCoordinateInput( QWidget *parent );
+
+    const QgsPointXY &getCoordinate() const { return mCoo; }
+    const QgsCoordinateReferenceSystem &getCrs() const { return mCrs; }
+    bool isEmpty() const { return mEmpty; }
+
+    void setCoordinate( const QgsPoint &coo, const QgsCoordinateReferenceSystem &crs );
+
+  private:
+    static const int sFormatRole = Qt::UserRole + 1;
+    static const int sAuthidRole = Qt::UserRole + 2;
+    QgsPointXY mCoo;
+    QgsCoordinateReferenceSystem mCrs;
+
+    bool mEmpty = true;
+    QLineEdit *mLineEdit;
+    QComboBox *mCrsCombo;
+
+  signals:
+    void coordinateEdited();
+    void coordinateChanged();
+
+  private slots:
+    void entryChanged();
+    void entryEdited();
+    void crsChanged();
+};
+
+#endif // KADASCOORDINATEINPUT_H
