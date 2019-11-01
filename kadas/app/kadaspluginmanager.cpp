23b33f1d23aabb73cfeaa6b16df9581b0555c332
Sandro Mani <manisandro@gmail.com>
2019-11-01 01:02:25 +0100

    Add copyright headers

--- kadas/app/kadaspluginmanager.cpp
+++ kadas/app/kadaspluginmanager.cpp
@@ -1,3 +1,19 @@
+/***************************************************************************
+    kadaspluginmanager.h
+    --------------------
+    copyright            : (C) 2019 by Marco Hugentobler
+    email                : marco at sourcepole dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
 #include <QDomDocument>
 #include <QMessageBox>
 #include <QSet>
