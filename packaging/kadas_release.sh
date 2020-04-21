d1b144e8e2f7b7659bb1b91829eba9fc98767ff5
Aleix Pol <aleixpol@kde.org>
2020-04-21 03:02:25 +0200

    Install templates

--- packaging/kadas_release.sh
+++ packaging/kadas_release.sh
@@ -5,7 +5,6 @@ cd "$(dirname "$(readlink -f $0)")"
 basedir=${1:-$PWD/..}
 distroot=$basedir/dist
 debugroot=$basedir/dist_debug
-filesdir=$basedir/packaging/files
 
 # Manually merge origins.txt
 cat $distroot/origins.txt $basedir/build_mingw64/dist/usr/x86_64-w64-mingw32/sys-root/mingw/origins.txt | sort | uniq > origins.txt
@@ -29,9 +28,6 @@ rm -f $distroot/share/qgis/FindQGIS.cmake
 
 # Remove unused translations
 find $distroot/share/qgis/i18n/ -type f -not -regex '^.*/qgis_\(de\|it\|fr\)\.qm$' -delete
-
-# Copy settings, templates, etc.
-cp -a $filesdir/* $distroot/share/kadas/
 
 # Copy proj datum grid shifts
 wget -O $distroot/share/proj/CHENyx06a.gsb https://github.com/OSGeo/proj-datumgrid/raw/master/europe/CHENyx06a.gsb
