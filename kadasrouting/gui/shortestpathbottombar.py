9dccada040a33243a01951f502dc427912eb63fc
Ismail Sunni <imajimatika@gmail.com>
2020-06-24 15:18:42 +0200

    Set icons.

--- kadasrouting/gui/shortestpathbottombar.py
+++ kadasrouting/gui/shortestpathbottombar.py
@@ -10,12 +10,25 @@ WIDGET, BASE = uic.loadUiType(os.path.join(os.path.dirname(__file__), 'shortestp
 class ShortestPathBottomBar(KadasBottomBar, WIDGET):
 
     def __init__(self, canvas, action):
-        KadasBottomBar.__init__(self, canvas, "orange")        
+        KadasBottomBar.__init__(self, canvas, "orange")
         self.setupUi(self)
-        self.setStyleSheet("QFrame { background-color: orange; }")        
+        self.setStyleSheet("QFrame { background-color: orange; }")
         self.action = action
+
+        # Config buttons (icons, actions)
+        self.btnGPSOrigin.setIcon(QIcon(":/kadas/icons/gps"))
+        self.btnGPSDestination.setIcon(QIcon(":/kadas/icons/gps"))
+        self.btnGPSWaypoints.setIcon(QIcon(":/kadas/icons/gps"))
+
+        self.btnMapToolOrigin.setIcon(QIcon(":/kadas/icons/pick"))
+        self.btnMapToolDestination.setIcon(QIcon(":/kadas/icons/pick"))
+        self.btnMapToolWaypoints.setIcon(QIcon(":/kadas/icons/pick"))
+
+        self.btnAddWaypoints.setIcon(QIcon(":/kadas/icons/add"))
+
+
         self.btnClose.setIcon(QIcon(":/kadas/icons/close"))
         self.btnClose.clicked.connect(self.action.toggle)
+
         self.originSearchBox = addSearchBox(self.originWidget)
 
-        
