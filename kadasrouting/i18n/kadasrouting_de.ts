9c861d9820befa233483c96edfffee314917aad4
Ismail Sunni <imajimatika@gmail.com>
2020-08-07 16:10:34 +0200

    Update translation.

--- kadasrouting/i18n/kadasrouting_de.ts
+++ kadasrouting/i18n/kadasrouting_de.ts
@@ -1,131 +1,130 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.0">
 <context>
     <name>KadasPluginManagerBase</name>
     <message>
         <location filename="tspbottombar.ui" line="14"/>
         <source>Frame</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="41"/>
         <source>Fastest</source>
-        <translation type="unfinished"></translation>
+        <translation>Am schnellsten</translation>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="51"/>
         <source>Shortest</source>
-        <translation type="unfinished"></translation>
+        <translation>kürzeste</translation>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="57"/>
         <source>From</source>
-        <translation type="unfinished"></translation>
+        <translation>von</translation>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="71"/>
         <source>Route type</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="102"/>
         <source>To</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="112"/>
         <source>Vehicle type</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="119"/>
         <source>Waypoints</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="167"/>
         <source>Clear</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="optimalroutebottombar.ui" line="174"/>
         <source>Reverse</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="153"/>
         <source>Calculate</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="26"/>
         <source>Draw CAN-NOT Area</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="33"/>
         <source>Draw MUST Area</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="57"/>
         <source>Out</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="64"/>
         <source>In</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="93"/>
         <source>Reset</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="140"/>
         <source>...</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="tspbottombar.ui" line="130"/>
         <source>Buffer</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="64"/>
         <source>Basename</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="77"/>
         <source>isochrones</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="84"/>
         <source>Mode</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="94"/>
         <source>Intervals</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="107"/>
         <source>10;20;30</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="114"/>
         <source>With</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.ui" line="124"/>
         <source>Remove previous isoline</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -133,12 +132,12 @@
     <message>
         <location filename="reachabilitybottombar.py" line="67"/>
         <source>Isochrone</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="reachabilitybottombar.py" line="68"/>
         <source>Isodistance</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 <context>
@@ -146,17 +145,17 @@
     <message>
         <location filename="plugin.py" line="40"/>
         <source>Routing</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="plugin.py" line="48"/>
         <source>Reachability</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
     <message>
         <location filename="plugin.py" line="58"/>
         <source>TSP</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished"/>
     </message>
 </context>
 </TS>
