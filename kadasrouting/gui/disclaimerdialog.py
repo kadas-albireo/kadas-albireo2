b25dc2d4a32afe0b9870105832ebe69c8d359be1
volaya <volayaf@gmail.com>
2020-09-14 11:03:41 +0200

    made disclaimer dialog multilingual

--- kadasrouting/gui/disclaimerdialog.py
+++ kadasrouting/gui/disclaimerdialog.py
@@ -5,13 +5,21 @@ from qgis.PyQt import uic
 
 from qgis.PyQt.QtCore import QSettings
 
+from kadasrouting.utilities import localeName
+
 WIDGET, BASE = uic.loadUiType(os.path.join(os.path.dirname(__file__), "disclaimerdialog.ui"))
 
 class DisclaimerDialog(BASE, WIDGET):
 
-    def __init__(self, parent=None):
+    def __init__(self, parent=None):    	
         super(DisclaimerDialog, self).__init__(parent)
         self.setupUi(self)
+        indexes = ["en", "de", "fr", "it"]
+        try:
+        	index = indexes.index(localeName())
+        except ValueError:
+        	index = 0 
+        self.stackedWidget.setCurrentIndex(index)
         self.btnClose.clicked.connect(self.closeClicked)
 
     def closeClicked(self):
