2b45a47e9590d1b55605c8f463d8c58a5cbb4fe9
Ismail Sunni <imajimatika@gmail.com>
2020-09-21 16:03:47 +0200

    Tidy up import.

--- kadasrouting/gui/autocompletewidget.py
+++ kadasrouting/gui/autocompletewidget.py
@@ -1,18 +1,21 @@
 import json
-from PyQt5 import QtCore, QtGui, QtWidgets, QtNetwork
-from PyQt5.QtCore import Qt
+
+from PyQt5.QtCore import Qt, pyqtSignal, QEventLoop, pyqtSlot, QUrl, QUrlQuery
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
+from PyQt5.QtNetwork import QNetworkAccessManager, QNetworkRequest, QNetworkReply
+from PyQt5.QtWidgets import QCompleter, QLineEdit
 
 
-class SuggestionPlaceModel(QtGui.QStandardItemModel):
-    finished = QtCore.pyqtSignal()
-    error = QtCore.pyqtSignal(str)
+class SuggestionPlaceModel(QStandardItemModel):
+    finished = pyqtSignal()
+    error = pyqtSignal(str)
 
     def __init__(self, parent=None):
         super(SuggestionPlaceModel, self).__init__(parent)
-        self._manager = QtNetwork.QNetworkAccessManager(self)
+        self._manager = QNetworkAccessManager(self)
         self._reply = None
 
-    @QtCore.pyqtSlot(str)
+    @pyqtSlot(str)
     def search(self, text):
         self.clear()
         if self._reply is not None:
@@ -21,32 +24,32 @@ class SuggestionPlaceModel(QtGui.QStandardItemModel):
             r = self.create_request(text)
             self._reply = self._manager.get(r)
             self._reply.finished.connect(self.on_finished)
-        loop = QtCore.QEventLoop()
+        loop = QEventLoop()
         self.finished.connect(loop.quit)
         loop.exec_()
 
     def create_request(self, text):
-        url = QtCore.QUrl("https://api3.geo.admin.ch/rest/services/api/SearchServer")
-        query = QtCore.QUrlQuery()
+        url = QUrl("https://api3.geo.admin.ch/rest/services/api/SearchServer")
+        query = QUrlQuery()
         query.addQueryItem("sr", "2056")
         query.addQueryItem("searchText", text)
         query.addQueryItem("lang", "en")
         query.addQueryItem("type", "locations")
         url.setQuery(query)
-        request = QtNetwork.QNetworkRequest(url)
+        request = QNetworkRequest(url)
         return request
 
-    @QtCore.pyqtSlot()
+    @pyqtSlot()
     def on_finished(self):
         reply = self.sender()
-        if reply.error() == QtNetwork.QNetworkReply.NoError:
+        if reply.error() == QNetworkReply.NoError:
             data = json.loads(reply.readAll().data())
             if data.get('status') != 'error':
                 for location in data['results']:
                     attributes = location.get('attrs', {})
                     label = attributes.get('label', 'Unknown label')
                     # Create standard item to store the data
-                    item = QtGui.QStandardItem(label)
+                    item = QStandardItem(label)
                     item.setData(attributes.get('lat'), Qt.UserRole)
                     item.setData(attributes.get('lon'), Qt.UserRole + 1)
                     item.setData(attributes.get('x'), Qt.UserRole + 2)
@@ -58,15 +61,15 @@ class SuggestionPlaceModel(QtGui.QStandardItemModel):
         reply.deleteLater()
         self._reply = None
 
-class Completer(QtWidgets.QCompleter):
+class Completer(QCompleter):
     def splitPath(self, path):
         self.model().search(path)
         return super(Completer, self).splitPath(path)
 
-class AutoCompleteWidget(QtWidgets.QLineEdit):
+class AutoCompleteWidget(QLineEdit):
     def __init__(self, parent=None):
         super(AutoCompleteWidget, self).__init__(parent)
         self._model = SuggestionPlaceModel(self)
-        completer = Completer(self, caseSensitivity=QtCore.Qt.CaseInsensitive)
+        completer = Completer(self, caseSensitivity=Qt.CaseInsensitive)
         completer.setModel(self._model)
         self.setCompleter(completer)
