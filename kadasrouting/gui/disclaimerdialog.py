8241091f87a226e12b3f2b44af4e71d73f3f1c67
Ismail Sunni <imajimatika@gmail.com>
2020-09-24 21:42:10 +0200

    Flake8 on gps and disclaimer

--- kadasrouting/gui/disclaimerdialog.py
+++ kadasrouting/gui/disclaimerdialog.py
@@ -1,5 +1,4 @@
 import os
-import json
 
 from qgis.PyQt import uic
 
@@ -9,16 +8,17 @@ from kadasrouting.utilities import localeName
 
 WIDGET, BASE = uic.loadUiType(os.path.join(os.path.dirname(__file__), "disclaimerdialog.ui"))
 
+
 class DisclaimerDialog(BASE, WIDGET):
 
-    def __init__(self, parent=None):    	
+    def __init__(self, parent=None):
         super(DisclaimerDialog, self).__init__(parent)
         self.setupUi(self)
         indexes = ["en", "de", "fr", "it"]
         try:
-        	index = indexes.index(localeName())
+            index = indexes.index(localeName())
         except ValueError:
-        	index = 0 
+            index = 0
         self.stackedWidget.setCurrentIndex(index)
         self.btnClose.clicked.connect(self.closeClicked)
 
