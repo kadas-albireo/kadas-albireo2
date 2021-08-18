2b06e529b148427af2845f7f28f1581a00998261
Sandro Mani <manisandro@gmail.com>
2021-08-18 15:34:57 +0200

    Change default format to png32 for mapserver layers

--- kadas/gui/catalog/kadasarcgisportalcatalogprovider.cpp
+++ kadas/gui/catalog/kadasarcgisportalcatalogprovider.cpp
@@ -403,7 +403,7 @@ void KadasArcGisPortalCatalogProvider::readAMSCapabilitiesDo()
       mimeDataUri.providerKey = "arcgismapserver";
       const ResultEntry &entry = ( *entries ) [layerName];
       mimeDataUri.name = entry.title;
-      QString format = filteredEncodings.isEmpty() || filteredEncodings.contains( "png" ) ? "png" : filteredEncodings.values().front();
+      QString format = filteredEncodings.isEmpty() || filteredEncodings.contains( "png32" ) ? "png32" : filteredEncodings.values().front();
       mimeDataUri.uri = QString( "crs='%1' format='%2' url='%3' layer='%4'" ).arg( crs.authid() ).arg( format ).arg( url ).arg( layerName );
       QMimeData *mimeData = QgsMimeDataUtils::encodeUriList( QgsMimeDataUtils::UriList() << mimeDataUri );
       mimeData->setProperty( "metadataUrl", entry.metadataUrl );
