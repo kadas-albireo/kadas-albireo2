341e994c9b8cb005f3e6de404a6b0f7f9aa7a7e0
Sandro Mani <manisandro@gmail.com>
2021-10-12 10:38:23 +0200

    Update mingw/build.sh to not fail if qt5/translations do not exist

--- scripts/mingwbuild.sh
+++ scripts/mingwbuild.sh
@@ -215,8 +215,7 @@ osgPlugins=$(basename $MINGWROOT/bin/osgPlugins-*)
 lnk $MINGWROOT/bin/$osgPlugins $installprefix/bin/$osgPlugins
 
 mkdir -p $installprefix/share/qt5/translations/
-#cp -a $MINGWROOT/share/qt5/translations/qt_*.qm  $installprefix/share/qt5/translations
-#cp -a $MINGWROOT/share/qt5/translations/qtbase_*.qm  $installprefix/share/qt5/translations
+find $MINGWROOT/share/qt5/translations/ \( -name 'qt_*.qm' -or -name 'qtbase_*.qm' \) -exec cp -a {} $installprefix/share/qt5/translations \;
 
 # Sort origins file
 cat $installprefix/origins.txt | sort | uniq > $installprefix/origins.new && mv $installprefix/origins.new $installprefix/origins.txt
