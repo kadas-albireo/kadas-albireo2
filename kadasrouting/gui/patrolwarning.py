74e0b902e5ff2c6f04bfa2781ffd8d5ef8fe2c87
Ismail Sunni <imajimatika@gmail.com>
2021-11-26 01:34:28 +0100

    Add content for de, it, fr

--- kadasrouting/gui/patrolwarning.py
+++ kadasrouting/gui/patrolwarning.py
@@ -30,9 +30,30 @@ class PatrolWarning(BASE, WIDGET):
             "third_item": "If the problem persists even for a smaller well-defined area then the algorithm is unable to find a connection for at least one of the enclosed nodes and the patrol can not be calculated for this area.",
         }
 
-        content_de = {}
-        content_fr = {}
-        content_it = {}
+        content_de = {
+            "title": "Warnung",
+            "description": "Die Berechnung der Patrouillen-Route von Punkt {start} nach Punkt {end} ist fehlgeschlagen. Bitte versuchen Sie eine der folgenden Optionen:",
+            # TODO: Use a list
+            "first_item": "Zeichnen Sie ein kleineres zu befahrendes Gebiet",
+            "second_item": "Zeichnen Sie das Gebiet so, dass es keine Hauptverkehrsstraßen kreuzt",
+            "third_item": "Wenn das Problem auch bei einem kleineren klar definierten Gebiet bestehen bleibt, ist der Algorithmus in dieser Version nicht in der Lage eine Verbindung für mindestens einen der eingeschlossenen Knotenpunkte zu finden und die Patrouille kann für dieses Gebiet nicht berechnet werden.",
+        }
+        content_fr = {
+            "title": "Atencion",
+            "description": "Le calcul de l'itinéraire de patrouille a échoué du point {start} au point {end}. Veuillez essayer l'une des options suivantes :",
+            # TODO: Use a list
+            "first_item": "Redessinez une zone de patrouille plus petite.",
+            "second_item": "Évitez de dessiner une zone de patrouille qui croise des routes principales.",
+            "third_item": "Si le problème persiste même pour une zone plus petite et bien définie, cela signifie que l'algorithme est incapable de trouver une connexion pour au moins un des nœuds inclus et que la patrouille ne peut pas être calculée pour cette zone.",
+        }
+        content_it = {
+            "title": "Attenzione",
+            "description": "Il calcolo del percorso di ronda non è riuscito dal punto {start} al punto {end}. Prova una delle seguenti opzioni:",
+            # TODO: Use a list
+            "first_item": "Ridisegnare un'area di pattuglia più piccola",
+            "second_item": "Evitare di disegnare un'area di pattuglia che intersechi le strade principali",
+            "third_item": "Se il problema persiste anche per un'area più piccola e ben definita, allora l'algoritmo non è in grado di trovare una connessione per almeno uno dei nodi racchiusi e la ronda non può essere calcolata per quest'area.",
+        }
 
         self.contents = {
             "en": content_en,
