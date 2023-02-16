d78d0a8f2fd1458a34ac91ac783c0cb12f2eef97
Sandro Mani <manisandro@gmail.com>
2023-02-16 20:00:56 +0100

    Update translations, add note regarding possibility of multiple rises and sets

--- i18n/kadas_ephem_fr.ts
+++ i18n/kadas_ephem_fr.ts
@@ -12,79 +12,109 @@
 <context>
     <name>EphemToolWidget</name>
     <message>
-        <location filename="../ephem_tool.py" line="84"/>
+        <location filename="../ephem_tool.py" line="94"/>
         <source>Close</source>
         <translation>Fermer</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="174"/>
+        <location filename="../ui_EphemToolWidget.py" line="210"/>
         <source>Date and time:</source>
         <translation>Date et heure:</translation>
     </message>
     <message>
         <location filename="../ui_EphemToolWidget.py" line="171"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Calculate Ephemeris&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Calculer ephémérides&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="obsolete">&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;&amp;lt;span style=&amp;quot; font-weight:600;&amp;quot;&amp;gt;Calculer ephémérides&amp;lt;/span&amp;gt;&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="186"/>
+        <location filename="../ui_EphemToolWidget.py" line="224"/>
         <source>Azimuth / Elevation:</source>
         <translation>Azimut / Élévation:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="179"/>
+        <location filename="../ui_EphemToolWidget.py" line="217"/>
         <source>Zenith:</source>
         <translation>Zenit:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="183"/>
+        <location filename="../ui_EphemToolWidget.py" line="221"/>
         <source>Rise:</source>
         <translation>Ascension:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="185"/>
+        <location filename="../ui_EphemToolWidget.py" line="223"/>
         <source>Set:</source>
         <translation>Descente:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="182"/>
+        <location filename="../ui_EphemToolWidget.py" line="220"/>
         <source>Sun</source>
         <translation>Soleil</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="184"/>
+        <location filename="../ui_EphemToolWidget.py" line="222"/>
         <source>Phase:</source>
         <translation>Phase:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="187"/>
+        <location filename="../ui_EphemToolWidget.py" line="225"/>
         <source>Moon</source>
         <translation>Lune</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="173"/>
+        <location filename="../ui_EphemToolWidget.py" line="212"/>
         <source>Position:</source>
         <translation>Position:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="177"/>
+        <location filename="../ui_EphemToolWidget.py" line="213"/>
         <source>Click on the map to select a position</source>
         <translation>Cliquer sur la carte pour sélectionner une position</translation>
     </message>
     <message>
-        <location filename="../ephem_tool.py" line="94"/>
+        <location filename="../ephem_tool.py" line="109"/>
         <source>Calculating...</source>
         <translation>Calcul...</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="175"/>
+        <location filename="../ui_EphemToolWidget.py" line="209"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Ephemeris&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Ephémérides&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;&amp;lt;span style=&amp;quot; font-weight:600;&amp;quot;&amp;gt;Ephémérides&amp;lt;/span&amp;gt;&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="176"/>
+        <location filename="../ui_EphemToolWidget.py" line="211"/>
         <source>Consider relief</source>
         <translation>Considérer le terrain</translation>
+    </message>
+    <message>
+        <location filename="../ephem_tool.py" line="101"/>
+        <source>System time</source>
+        <translation>Heure du système</translation>
+    </message>
+    <message>
+        <location filename="../ephem_tool.py" line="102"/>
+        <source>UTC</source>
+        <translation>UTC</translation>
+    </message>
+    <message>
+        <location filename="../ephem_tool.py" line="103"/>
+        <source>Local time at position</source>
+        <translation>Heure locale à la position</translation>
+    </message>
+    <message>
+        <location filename="../ui_EphemToolWidget.py" line="214"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Timezone:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Fuseau horaire:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui_EphemToolWidget.py" line="215"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="../ui_EphemToolWidget.py" line="226"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Note: In locations with steep relief and/or large height differences over a short distance, several rises and sets are possible, but only one result will be displayed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Remarque : dans les endroits où le relief est abrupt et/ou les différences de hauteur importantes sur une courte distance, plusieurs ascensions et descentes sont possibles, mais un seul résultat sera affiché.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 </TS>
--- i18n/kadas_ephem_it.ts
+++ i18n/kadas_ephem_it.ts
@@ -12,79 +12,114 @@
 <context>
     <name>EphemToolWidget</name>
     <message>
-        <location filename="../ephem_tool.py" line="84"/>
+        <location filename="../ephem_tool.py" line="94"/>
         <source>Close</source>
         <translation>Chiudi</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="174"/>
+        <location filename="../ui_EphemToolWidget.py" line="210"/>
         <source>Date and time:</source>
         <translation>Data e ora:</translation>
     </message>
     <message>
         <location filename="../ui_EphemToolWidget.py" line="171"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Calculate Ephemeris&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Calcola effemeridi&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="obsolete">&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;&amp;lt;span style=&amp;quot; font-weight:600;&amp;quot;&amp;gt;Calcola effemeridi&amp;lt;/span&amp;gt;&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="186"/>
+        <location filename="../ui_EphemToolWidget.py" line="224"/>
         <source>Azimuth / Elevation:</source>
         <translation>Azimut / Elevazione:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="179"/>
+        <location filename="../ui_EphemToolWidget.py" line="217"/>
         <source>Zenith:</source>
         <translation>Zenit:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="183"/>
+        <location filename="../ui_EphemToolWidget.py" line="221"/>
         <source>Rise:</source>
         <translation>Sorge:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="185"/>
+        <location filename="../ui_EphemToolWidget.py" line="223"/>
         <source>Set:</source>
         <translation>Tramonta:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="182"/>
+        <location filename="../ui_EphemToolWidget.py" line="220"/>
         <source>Sun</source>
         <translation>Sole</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="184"/>
+        <location filename="../ui_EphemToolWidget.py" line="222"/>
         <source>Phase:</source>
         <translation>Fase:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="187"/>
+        <location filename="../ui_EphemToolWidget.py" line="225"/>
         <source>Moon</source>
         <translation>Luna</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="173"/>
+        <location filename="../ui_EphemToolWidget.py" line="212"/>
         <source>Position:</source>
         <translation>Posizione:</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="177"/>
+        <location filename="../ui_EphemToolWidget.py" line="213"/>
         <source>Click on the map to select a position</source>
         <translation>Seleziona una posizione sulla mappa</translation>
     </message>
     <message>
-        <location filename="../ephem_tool.py" line="94"/>
+        <location filename="../ephem_tool.py" line="109"/>
         <source>Calculating...</source>
         <translation>Calcolando...</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="175"/>
+        <location filename="../ui_EphemToolWidget.py" line="209"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Ephemeris&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Effemeridi&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&amp;lt;html&amp;gt;&amp;lt;head/&amp;gt;&amp;lt;body&amp;gt;&amp;lt;p&amp;gt;&amp;lt;span style=&amp;quot; font-weight:600;&amp;quot;&amp;gt;Effemeridi&amp;lt;/span&amp;gt;&amp;lt;/p&amp;gt;&amp;lt;/body&amp;gt;&amp;lt;/html&amp;gt;</translation>
     </message>
     <message>
-        <location filename="../ui_EphemToolWidget.py" line="176"/>
+        <location filename="../ui_EphemToolWidget.py" line="211"/>
         <source>Consider relief</source>
         <translation>Considerare il rilievo</translation>
+    </message>
+    <message>
+        <location filename="../ephem_tool.py" line="101"/>
+        <source>System time</source>
+        <translation>Ora del sistema</translation>
+    </message>
+    <message>
+        <location filename="../ephem_tool.py" line="102"/>
+        <source>UTC</source>
+        <translation>UTC</translation>
+    </message>
+    <message>
+        <location filename="../ephem_tool.py" line="103"/>
+        <source>Local time at position</source>
+        <translation>Ora locale alla posizione</translation>
+    </message>
+    <message>
+        <location filename="../ui_EphemToolWidget.py" line="214"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Timezone:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Fuso orario:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui_EphemToolWidget.py" line="215"/>
+        <source>-</source>
+        <translation>-</translation>
+    </message>
+    <message>
+        <location filename="../ui_EphemToolWidget.py" line="225"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Note: In situations with high relief energy (steep relief, large height differences over a short distance), several rises and sets are possible, but only one result is displayed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="obsolete">Nota: in situazioni di elevata energia di rilievo (rilievo ripido, grandi differenze di altezza su una breve distanza), sono possibili diversi innalzamenti e insiemi, ma viene visualizzato un solo risultato.</translation>
+    </message>
+    <message>
+        <location filename="../ui_EphemToolWidget.py" line="226"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Note: In locations with steep relief and/or large height differences over a short distance, several rises and sets are possible, but only one result will be displayed.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;Nota: nelle località con rilievi ripidi e/o grandi differenze di altezza su una breve distanza, sono possibili più albe e più tramonti, ma verrà visualizzato un solo risultato.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
 </context>
 </TS>
