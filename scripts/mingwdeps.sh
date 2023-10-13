ffaede619a430ee1a90b70c21297a64bdd62212b
Sandro Mani <manisandro@gmail.com>
2023-10-13 13:33:50 +0200

    Switch back to dnf for CI

--- scripts/mingwdeps.sh
+++ scripts/mingwdeps.sh
@@ -2,7 +2,7 @@
 
 wget -O /etc/yum.repos.d/smani-mingw-extras-fedora-rawhide.repo https://copr.fedorainfracloud.org/coprs/smani/mingw-extras/repo/fedora-rawhide/smani-mingw-extras-fedora-rawhide.repo && \
 wget -O /etc/yum.repos.d/smani-osgearth-2.10-fedora-rawhide.repo https://copr.fedorainfracloud.org/coprs/smani/osgearth-2.10/repo/fedora-rawhide/smani-osgearth-2.10-fedora-rawhide.repo && \
-dnf5 install -y \
+dnf install -y \
   mingw64-dlfcn \
   mingw64-exiv2 \
   mingw64-gcc-c++ \
