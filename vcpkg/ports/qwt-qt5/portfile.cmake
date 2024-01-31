cc9a7dab3c245490edcdee8baefd70c9c1f9747d
Matthias Kuhn <matthias@opengis.ch>
2024-01-31 09:55:05 +0100

    More ports

---
+++ vcpkg/ports/qwt-qt5/portfile.cmake
@@ -0,0 +1,33 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL "https://git.code.sf.net/p/qwt/git"
+    REF "06d6822b595b70c9fd567a4fe0d835759bf271fe"
+    FETCH_REF qwt-6.2
+    PATCHES 
+        config.patch
+        fix_dll_install.patch
+)
+
+string(COMPARE EQUAL  "${VCPKG_LIBRARY_LINKAGE}" "dynamic" IS_DYNAMIC)
+set(OPTIONS "")
+if(IS_DYNAMIC)
+    set(OPTIONS "QWT_CONFIG+=QwtDll")
+endif()
+set(VCPKG_QMAKE_COMMAND "${CURRENT_INSTALLED_DIR}/tools/qt5/bin/qmake")
+vcpkg_qmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    QMAKE_OPTIONS
+        ${OPTIONS}
+        "CONFIG-=debug_and_release"
+        "CONFIG+=create_prl"
+        "CONFIG+=link_prl"
+)
+vcpkg_qmake_install()
+vcpkg_copy_pdbs()
+
+if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
+    file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
+endif()
+
+# Handle copyright
+file(INSTALL "${SOURCE_PATH}/COPYING" DESTINATION "${CURRENT_PACKAGES_DIR}/share/${PORT}" RENAME copyright)
