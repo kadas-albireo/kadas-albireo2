55d932fcaec4cbaadbbc05c6847051e1708381be
Geo Ninja <37747133+geo-ninja@users.noreply.github.com>
2024-06-10 13:51:26 +0000

    Synchronize translations

---
+++ share/python/plugins/kadas_gpkg/i18n/kadas_gpkg_it.ts
@@ -0,0 +1,232 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="it">
+<context>
+    <name>KadasGpkg</name>
+    <message>
+        <location filename="../kadas_gpkg.py" line="63"/>
+        <source>GPKG Export</source>
+        <translation>Esportazione GPKG</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg.py" line="69"/>
+        <source>GPKG Import</source>
+        <translation>Importa GPKG</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg.py" line="73"/>
+        <source>GPKG</source>
+        <translation>GPKG</translation>
+    </message>
+</context>
+<context>
+    <name>KadasGpkgExport</name>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="183"/>
+        <source>Error</source>
+        <translation>Errore</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="97"/>
+        <source>Unable to create or open output file</source>
+        <translation>Impossibile creare or aprire il file di output</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="100"/>
+        <source>Writing %s...</source>
+        <translation>Scrivendo %s...</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="100"/>
+        <source>Cancel</source>
+        <translation>Annulla</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="191"/>
+        <source>GPKG Export</source>
+        <translation>Esportazione GPKG</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="129"/>
+        <source>The operation was canceled.</source>
+        <translation>L&apos;exportazione è stata annullata.</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="148"/>
+        <source>Invalid project</source>
+        <translation>Progetto non valido</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="183"/>
+        <source>Unable to create output file</source>
+        <translation>Impossibile creare il file di output</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="187"/>
+        <source>GPKG export completed</source>
+        <translation>Esportazione GPKG completata</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="191"/>
+        <source>The following layers were not exported to the GeoPackage:
+- %s</source>
+        <translation>I layer seguenti non sono stati scritti nel GeoPackage:
+- %s</translation>
+    </message>
+</context>
+<context>
+    <name>KadasGpkgExportBase</name>
+    <message>
+        <location filename="../kadas_gpkg_export_base.py" line="105"/>
+        <source>Writing %s</source>
+        <translation>Scrivendo %s...</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export_base.py" line="100"/>
+        <source>Write failed: error %d (%s)</source>
+        <translation>Errore di scrittura %d (%s)</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export_base.py" line="144"/>
+        <source>Write failed: error %d</source>
+        <translation>Errore di scrittura %d</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export_base.py" line="155"/>
+        <source>Computing pyramids, please wait...</source>
+        <translation>Calcolo delle piramidi, attendere...</translation>
+    </message>
+</context>
+<context>
+    <name>KadasGpkgExportDialog</name>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="38"/>
+        <source>Select GPKG File...</source>
+        <translation>Seleziona il file GPKG...</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_export.py" line="38"/>
+        <source>GPKG Database (*.gpkg)</source>
+        <translation>Database GPKG (*.gpkg)</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="104"/>
+        <source>GPKG Project Export</source>
+        <translation>Esporta progetto a GPKG</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="105"/>
+        <source>Output file:</source>
+        <translation>File di output:</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="106"/>
+        <source>Browse</source>
+        <translation>Sfoglia</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="107"/>
+        <source>Clear existing GeoPackage before exporting</source>
+        <translation>Rimuovi il GeoPackage esistente prima di esportare</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="108"/>
+        <source>The project, including embedded layers (redlining, symbols, pictures, ...), will be written to the GeoPackage.</source>
+        <translation>Il progetto, inclusi i layer in esso contenuti (redlining, symboli, immagini, ...), sarà scritto nel GeoPackage.</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="109"/>
+        <source>Additionally, the following layers will be added to the GeoPackage:</source>
+        <translation>In più, i seguenti layer saranno aggiunti al GeoPackage:</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="110"/>
+        <source>&lt;small&gt;&lt;i&gt;Layers already part of the output GeoPackage are disabled.&lt;/i&gt;&lt;/small&gt;</source>
+        <translation>&lt;small&gt;&lt;i&gt;Layer già contenuti nel GeoPackage sono disattivati.&lt;/i&gt;&lt;/small&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="111"/>
+        <source>&lt;small&gt;&lt;i&gt;Layers with unknown size or larger than 50 MB are deselected by default.&lt;/i&gt;&lt;/span&gt;</source>
+        <translation>&lt;small&gt;&lt;i&gt;Layer più grandi di 50 MB sono disattivati come impostazione predefinita.&lt;/i&gt;&lt;/small&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="112"/>
+        <source>Generate pyramids (slow)</source>
+        <translation>Genera piramidi (lento)</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_export_dialog.py" line="113"/>
+        <source>Specify raster export scale:</source>
+        <translation>Specificare scala di esportazione per layer raster:</translation>
+    </message>
+</context>
+<context>
+    <name>KadasGpkgImport</name>
+    <message>
+        <location filename="../kadas_gpkg_import.py" line="204"/>
+        <source>Import Errors</source>
+        <translation>Errori di importazione</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_import.py" line="204"/>
+        <source>The following layers could not be imported:%s</source>
+        <translation>Non è stato possibile importare i seguenti layer:%s</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_import.py" line="208"/>
+        <source>Save project?</source>
+        <translation>Salvare il progetto?</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_import.py" line="208"/>
+        <source>The project has unsaved changes. Do you want to save them before proceeding?</source>
+        <translation>Il progetto contiene dei cambiamenti non salvati. Salvarli prima di continuare?</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_import.py" line="245"/>
+        <source>GPKG import completed</source>
+        <translation>Importazione GPKG completata</translation>
+    </message>
+</context>
+<context>
+    <name>KadasGpkgImportDialog</name>
+    <message>
+        <location filename="../kadas_gpkg_import.py" line="46"/>
+        <source>Select GPKG File...</source>
+        <translation>Seleziona il file GPKG...</translation>
+    </message>
+    <message>
+        <location filename="../kadas_gpkg_import.py" line="46"/>
+        <source>GPKG Database (*.gpkg)</source>
+        <translation>Database GPKG (*.gpkg)</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_import_dialog.py" line="79"/>
+        <source>GPKG Import</source>
+        <translation>Importa GPKG</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_import_dialog.py" line="80"/>
+        <source>&lt;i&gt;The selected GPKG does not contain a QGIS project, and will be imported as a layer.&lt;/i&gt;</source>
+        <translation>&lt;i&gt;Il GPKG selezionato non contiene un progetto QGIS, e sarà pertanto importato come layer.&lt;/i&gt;</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_import_dialog.py" line="81"/>
+        <source>Input file:</source>
+        <translation>File di input:</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_import_dialog.py" line="82"/>
+        <source>Browse</source>
+        <translation>Sfoglia</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_import_dialog.py" line="83"/>
+        <source>Load entire project</source>
+        <translation>Caricare il progetto intero</translation>
+    </message>
+    <message>
+        <location filename="../ui_kadas_gpkg_import_dialog.py" line="84"/>
+        <source>Import only selected layers:</source>
+        <translation>Importare solamente i layer selezionati:</translation>
+    </message>
+</context>
+</TS>
---
+++ share/python/plugins/kadas_print/i18n/kadas_print_de.ts
@@ -0,0 +1,389 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="de">
+<context>
+    <name>CartoucheDialog</name>
+    <message>
+        <location filename="../CartoucheDialog.py" line="32"/>
+        <source>RESTRICTED</source>
+        <translation>INTERN</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="33"/>
+        <source>CONFIDENTIAL</source>
+        <translation>VERTRAULICH</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="34"/>
+        <source>SECRET</source>
+        <translation>GEHEIM</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="41"/>
+        <source>CLASSIFICATION</source>
+        <translation>KLASSIFIZIERUNG</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="59"/>
+        <source>Export</source>
+        <translation>Exportieren</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="60"/>
+        <source>Import</source>
+        <translation>Importieren</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="212"/>
+        <source>Export cartouche</source>
+        <translation>Kartousche exportieren</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="227"/>
+        <source>XML Files (*.xml);;</source>
+        <translation>XML Dateien (*.xml);;</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="221"/>
+        <source>Export failed</source>
+        <translation>Export gescheitert</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="221"/>
+        <source>Unable to write to file.</source>
+        <translation>Die Datei konnte nicht geschrieben werden.</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="227"/>
+        <source>Import cartouche</source>
+        <translation>Kartousche importieren</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="241"/>
+        <source>Import failed</source>
+        <translation>Import gescheitert</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="237"/>
+        <source>Unable to read file.</source>
+        <translation>Die Datei konnte nicht gelesen werden.</translation>
+    </message>
+    <message>
+        <location filename="../CartoucheDialog.py" line="241"/>
+        <source>The file does not appear to contain valid cartouche data.</source>
+        <translation>Die Datei scheint keine gültige Kartousche zu enthalten.</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="126"/>
+        <source>Map Cartouche</source>
+        <translation>Karten Kartusche</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="127"/>
+        <source>Exercise</source>
+        <translation>Übung</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="128"/>
+        <source>Course name</source>
+        <translation>Kursbezeichnung</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="129"/>
+        <source>Exercise name</source>
+        <translation>Übungsbezeichnung</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="130"/>
+        <source>Document name</source>
+        <translation>Dokumentname</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="131"/>
+        <source>Exercise organisation</source>
+        <translation>Übungsorganisation</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="132"/>
+        <source>Annex name</source>
+        <translation>Beilagebezeichnung</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="133"/>
+        <source>Title of cartouche</source>
+        <translation>Kartuschentitel</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="134"/>
+        <source>Place, date</source>
+        <translation>Ort, Datum</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="135"/>
+        <source>&quot;CODE NAME&quot;</source>
+        <translation>&amp;quot;DECKNAME&amp;quot;</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="136"/>
+        <source>Unit</source>
+        <translation>Truppenbezeichnung</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_cartouchedialog.py" line="137"/>
+        <source>Scale information</source>
+        <translation>Massstabbezeichnung</translation>
+    </message>
+</context>
+<context>
+    <name>PrintDialog</name>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="272"/>
+        <source>Print</source>
+        <translation>Drucken</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="273"/>
+        <source>Border:</source>
+        <translation>Rand:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="274"/>
+        <source>Extent:</source>
+        <translation>Ausschnitt:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="275"/>
+        <source>Layout:</source>
+        <translation>Vorlage:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="276"/>
+        <source>Title:</source>
+        <translation>Titel:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="277"/>
+        <source>Scalebar</source>
+        <translation>Massstabbalken</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="278"/>
+        <source>Legend</source>
+        <translation>Legende</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="279"/>
+        <source>File format:</source>
+        <translation>Dateiformat:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="280"/>
+        <source>Map cartouche</source>
+        <translation>Kartenkartusche</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="281"/>
+        <source>Scale:</source>
+        <translation>Massstab:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="282"/>
+        <source>...</source>
+        <translation>...</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="283"/>
+        <source>Only meter based coordinate systems are supported in variable extent mode.</source>
+        <translation>Nur Meterbasierte Koordinatensysteme werden im variablen Ausschnitt Modus unterstützt</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="284"/>
+        <source>X Max.:</source>
+        <translation>X Max.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="285"/>
+        <source>X Min.:</source>
+        <translation>X Min.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="286"/>
+        <source>Y Max.:</source>
+        <translation>Y Max.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="287"/>
+        <source>Y Min.:</source>
+        <translation>Y Min.:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="288"/>
+        <source> mm</source>
+        <translation>mm</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="289"/>
+        <source>Grid</source>
+        <translation>Gitter</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="290"/>
+        <source>Coordinate system:</source>
+        <translation>Koordinatensystem:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="291"/>
+        <source>Interval X:</source>
+        <translation>X Intervall:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="292"/>
+        <source>Coordinate labels</source>
+        <translation>Koordinatenbeschriftungen</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="293"/>
+        <source>Interval Y:</source>
+        <translation>Y Intervall:</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="294"/>
+        <source>Edit map cartouche</source>
+        <translation>Kartenkartusche anpassen</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printdialog.py" line="295"/>
+        <source>Configure</source>
+        <translation>Anpassen</translation>
+    </message>
+</context>
+<context>
+    <name>PrintLayoutManager</name>
+    <message>
+        <location filename="../ui/ui_printlayoutmanager.py" line="46"/>
+        <source>Print Layout Manager</source>
+        <translation>Druckzusammenstellungen verwalten</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printlayoutmanager.py" line="47"/>
+        <source>Import</source>
+        <translation>Importieren</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printlayoutmanager.py" line="48"/>
+        <source>Export</source>
+        <translation>Exportieren</translation>
+    </message>
+    <message>
+        <location filename="../ui/ui_printlayoutmanager.py" line="49"/>
+        <source>Remove</source>
+        <translation>Entfernen</translation>
+    </message>
+</context>
+<context>
+    <name>PrintTool</name>
+    <message>
+        <location filename="../PrintTool.py" line="44"/>
+        <source>Export</source>
+        <translation>Exportieren</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="46"/>
+        <source>Print</source>
+        <translation>Drucken</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="48"/>
+        <source>Advanced</source>
+        <translation>Fortgeschritten</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="56"/>
+        <source>PDF Document (*.pdf);;</source>
+        <translation>PDF Dokument (*.pdf);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="58"/>
+        <source>GeoPDF Document (*.geopdf);;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="60"/>
+        <source>JPG Image (*.jpg);;</source>
+        <translation>JPG Bild (*.jpg);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="62"/>
+        <source>BMP Image (*.bmp);;</source>
+        <translation>BMP Bild (*.bmp);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="64"/>
+        <source>PNG Image (*.png);;</source>
+        <translation>PNG Bild (*.png);;</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="379"/>
+        <source>Hidden</source>
+        <translation>Unsichtbar</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="379"/>
+        <source>Visible without layer legend</source>
+        <translation>Sichtbar, ohne Ebenenlegende</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="379"/>
+        <source>Visible with layer legend</source>
+        <translation>Sichtbar, mit Ebenenlegende</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="394"/>
+        <source>Configure legend</source>
+        <translation>Legende anpassen</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="396"/>
+        <source>Select layers to display in legend:</source>
+        <translation>Ebenen auswählen, die in der Legende erscheinen sollen:</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="547"/>
+        <source>Invalid layout</source>
+        <translation>Ungültige Druckzusammenstellung</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="547"/>
+        <source>The layout must have exactly one map item.</source>
+        <translation>Die Zusammenstellung muss genau ein Kartenelement haben.</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="615"/>
+        <source>Paper size: %.2f cm x %.2f cm</source>
+        <translation>Papierformat: %.2f cm x %.2f cm</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="635"/>
+        <source>Print Layout</source>
+        <translation>Vorlage</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="714"/>
+        <source>Print Failed</source>
+        <translation>Druck gescheitert</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="714"/>
+        <source>Failed to print the layout.</source>
+        <translation>Die Zusammenstellung konnte nicht gedruckt werden.</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="685"/>
+        <source>No Printers</source>
+        <translation>Keine Drucker</translation>
+    </message>
+    <message>
+        <location filename="../PrintTool.py" line="685"/>
+        <source>No printers were found.</source>
+        <translation>Keine Drucker wurden gefunden.</translation>
+    </message>
+</context>
+</TS>
