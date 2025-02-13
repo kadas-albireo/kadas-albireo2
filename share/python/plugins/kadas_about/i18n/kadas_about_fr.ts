2e6187acdffe3acc15a4937bb1957b81f0a37d5f
Geo Ninja <37747133+geo-ninja@users.noreply.github.com>
2025-02-13 01:36:37 +0000

    Synchronize translations

--- share/python/plugins/kadas_about/i18n/kadas_about_fr.ts
+++ share/python/plugins/kadas_about/i18n/kadas_about_fr.ts
@@ -2,17 +2,17 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../about_dialog.py" line="27"/>
+        <location filename="../about_dialog.py" line="25"/>
         <source>About %s</source>
         <translation>A propos de %s</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="36"/>
+        <location filename="../about_dialog.py" line="34"/>
         <source>&lt;b&gt;Version&lt;/b&gt;: %s (%s) - Based on QGIS %s</source>
         <translation type="unfinished"/>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="40"/>
+        <location filename="../about_dialog.py" line="38"/>
         <source>This software is released under the &lt;a href=&quot;http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html&quot;&gt;GNU Public License (GPL) Version 2&lt;/a&gt;</source>
         <translation type="unfinished"/>
     </message>
