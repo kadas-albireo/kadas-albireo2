50244fb3db608bc107c52b46e03aacd3c6a09487
Damiano Lombardi <lombardi.damiano@gmail.com>
2025-09-04 15:51:45 +0200

    and coordinates

--- tests/src/gui/search/testkadasalternategotolocatorfilter.cpp
+++ tests/src/gui/search/testkadasalternategotolocatorfilter.cpp
@@ -93,7 +93,7 @@ void TestKadasAlternateGotoLocatorFilter::testGoto_data()
 
   QTest::newRow( "osm.leaflet.OL" ) << QStringLiteral( "https://www.openstreetmap.org/#map=15/44.5546/6.4936" ) << Results( { { QObject::tr( "Go to 44.5546°N 6.4936°E at scale 1:22569" ), QgsPointXY( 6.4936, 44.5546 ), 22569.0 } } );
   QTest::newRow( "gmaps1" ) << QStringLiteral( "https://www.google.com/maps/@44.5546,6.4936,15.25z" ) << Results( { { QObject::tr( "Go to 44.5546°N 6.4936°E at scale 1:22569" ), QgsPointXY( 6.4936, 44.5546 ), 22569.0 } } );
-  QTest::newRow( "gmaps2" ) << QStringLiteral( "https://www.google.com/maps/@7.931768,80.8731272,688963m/data=!3m1!1e3" ) << Results( { { QObject::tr( "Go to 7.931768°N 80.8731272°E at scale 1:6.45343e+07" ), QgsPointXY( 81.0149, 7.8750 ) } } );
+  QTest::newRow( "gmaps2" ) << QStringLiteral( "https://www.google.com/maps/@7.931768,80.8731272,688963m/data=!3m1!1e3" ) << Results( { { QObject::tr( "Go to 7.931768°N 80.8731272°E at scale 1:6.45343e+07" ), QgsPointXY( 80.8731272, 7.931768 ) } } );
   QTest::newRow( "gmaps3" ) << QStringLiteral( "https://www.google.com/maps/@27.7132,85.3288,3a,75y,278.89h,90t/data=!3m8!1e1!3m6!1sAF1QipMrXuXozGc9x9bxx5uPl_3ys4H-rNVqMLr6EYLA!2e10!3e11!6shttps:%2F%2Flh5.googleusercontent.com%2Fp%2FAF1QipMrXuXozGc9x9bxx5uPl_3ys4H-rNVqMLr6EYLA%3Dw203-h100-k-no-pi2.869903-ya293.58762-ro-1.9255565-fo100!7i3840!8i1920" ) << Results( { { QObject::tr( "Go to 27.7132°N 85.3288°E at scale 1:282" ), QgsPointXY( 85.3288, 27.7132 ), 282.0 } } );
 }
 
