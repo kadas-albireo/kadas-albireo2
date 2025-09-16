b0d3542c32a57b366a596758a35f378ba56cbb4c
Damiano Lombardi <damiano@opengis.ch>
2025-09-16 14:17:53 +0200

    Add test for some layers

--- tests/src/gui/catalog/testkadascatalogbrowser.cpp
+++ tests/src/gui/catalog/testkadascatalogbrowser.cpp
@@ -1,7 +1,13 @@
 
 #include <QtTest/QTest>
 
+#include <qgis/qgslayertreemodel.h>
+#include <qgis/qgslayertreelayer.h>
+#include <qgis/qgslayertree.h>
+#include <qgis/qgsmaplayer.h>
+
 #include <kadas/test/kadastest.h>
+#include <kadas/app/kadasmainwindow.h>
 
 class TestKadasCatalogBrowser : public QObject
 {
@@ -10,6 +16,9 @@ class TestKadasCatalogBrowser : public QObject
   private slots:
     void initTestCase();
     void cleanupTestCase();
+
+    void testAddCatalogLayer();
+    void testAddCatalogLayer_data();
 };
 
 void TestKadasCatalogBrowser::initTestCase()
@@ -21,6 +30,46 @@ void TestKadasCatalogBrowser::initTestCase()
 
 void TestKadasCatalogBrowser::cleanupTestCase()
 {
+  KadasApplication *app = KadasApplication::instance();
+  KadasMainWindow *mainWindow = app->mainWindow();
+  QgsLayerTree *layerTree = mainWindow->layerTreeView()->layerTreeModel()->rootGroup();
+  layerTree->removeAllChildren();
+}
+
+void TestKadasCatalogBrowser::testAddCatalogLayer()
+{
+  QFETCH( QString, uri );
+
+  KadasApplication *app = KadasApplication::instance();
+  KadasMainWindow *mainWindow = app->mainWindow();
+  QgsLayerTree *layerTree = mainWindow->layerTreeView()->layerTreeModel()->rootGroup();
+  layerTree->removeAllChildren();
+
+  QVERIFY(QMetaObject::invokeMethod(mainWindow,
+                                    "addCatalogLayer",
+                                    Qt::DirectConnection,
+                                    Q_ARG(QgsMimeDataUtils::Uri, QgsMimeDataUtils::Uri(uri)),
+                                    Q_ARG(QString, ""),
+                                    Q_ARG(QVariantList, QVariantList())));
+
+  QCOMPARE( layerTree->findLayers().size(), 1 );
+
+  QgsLayerTreeLayer *firstLayerNode = dynamic_cast<QgsLayerTreeLayer *>( layerTree->children().at( 0 ) );
+  QVERIFY( firstLayerNode );
+  
+  QgsMapLayer *layer = firstLayerNode->layer();
+  QVERIFY( layer );
+  QVERIFY( layer->isValid() );
+}
+
+void TestKadasCatalogBrowser::testAddCatalogLayer_data()
+{
+  QTest::addColumn<QString>( "uri" );
+
+  QTest::newRow( "Apotheken" ) << QStringLiteral( "vector:arcgisfeatureserver:Apotheken:crs='EPSG\\:2056' url='https\\://geoinfo.op.intra2.admin.ch/arcgis/rest/services/O/ChPharmasuisseApotheken/MapServer'" );
+  QTest::newRow( "Armeeapotheke" ) << QStringLiteral( "vector:arcgisfeatureserver:Armeeapotheke:crs='EPSG\\:2056' url='https\\://geoinfo.op.intra2.admin.ch/arcgis/rest/services/R4/ChVbsArmeeapotheke/MapServer'" );
+  QTest::newRow( "Administrative Grenzen" ) << QStringLiteral( "vector:arcgisfeatureserver:Administrative Grenzen / Gemeindegrenzen:crs='EPSG\\:2056' url='https\\://geoinfo.op.intra2.admin.ch/arcgis/rest/services/D/ChSwisstopoSwissboundaries3dGemeindeFlaecheFill/MapServer'" );
+  QTest::newRow( "Amtliches Strassenverzeichnis" ) << QStringLiteral( "vector:arcgisfeatureserver:Amtliches Strassenverzeichnis:crs='EPSG\\:2056' url='https\\://geoinfo.op.intra2.admin.ch/arcgis/rest/services/B/ChSwisstopoAmtlichesStrassenverzeichnis/MapServer'" );
 }
 
 KADASTEST_MAIN( TestKadasCatalogBrowser )
