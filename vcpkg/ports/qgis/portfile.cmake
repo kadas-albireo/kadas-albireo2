4ad03209010eee4efa8bef035f344c3e588dc4d1
Matthias Kuhn <matthias@opengis.ch>
2025-11-28 06:28:11 +0100

    Remove sipconcat.patch from QGIS portfile

--- vcpkg/ports/qgis/portfile.cmake
+++ vcpkg/ports/qgis/portfile.cmake
@@ -32,7 +32,6 @@ vcpkg_from_github(
   flagDegreesUseUntranslatedStringSuffix.patch # https://jira.swisstopo.ch/browse/MGDIGRE_SB-1272
   wcsSpatialExtentSettings.patch # https://jira.swisstopo.ch/browse/MGDIGRE_SB-1201
   mac_install_images.patch
-  sipconcat.patch
 )
 
 file(REMOVE ${SOURCE_PATH}/cmake/FindGDAL.cmake)
