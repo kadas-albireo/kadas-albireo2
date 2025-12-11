d15560b367e436453d4ce8f498e833ea50335ccb
Denis Rouzaud <denis.rouzaud@gmail.com>
2025-12-11 10:32:36 +0100

    Fix translation in 2.3 (#411)
    * add en TS file
    
    * add en TS file
    
    * Added missing links in About menu (#410)
    
    * Add files via upload
    
    * Refactor AboutDialog class for better readability
    
    ---------
    
    Co-authored-by: Denis Rouzaud <denis.rouzaud@gmail.com>
    
    * Synchronize translations
    
    * Synchronize translations
    
    ---------
    
    Co-authored-by: shenriod <stephane@henriod.info>
    Co-authored-by: Geo Ninja <37747133+geo-ninja@users.noreply.github.com>

--- share/python/plugins/kadas_about/i18n/kadas_about_de.ts
+++ share/python/plugins/kadas_about/i18n/kadas_about_de.ts
@@ -2,24 +2,49 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="../about_dialog.py" line="27"/>
+        <location filename="../about_dialog.py" line="26"/>
         <source>About %s</source>
         <translation>Über %s</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="36"/>
+        <location filename="../about_dialog.py" line="35"/>
         <source>&lt;b&gt;Version&lt;/b&gt;: %s (%s) - Based on QGIS %s</source>
         <translation>&lt;b&gt;Version&lt;/b&gt;: %s (%s) - Basierend auf QGIS %s</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="40"/>
+        <location filename="../about_dialog.py" line="39"/>
         <source>This software is released under the &lt;a href=&quot;http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html&quot;&gt;GNU Public License (GPL) Version 2&lt;/a&gt;</source>
         <translation>Diese Software steht unter der &lt;a href=&quot;http://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html&quot;&gt;GNU Public License (GPL) Version 2&lt;/a&gt;</translation>
     </message>
     <message>
-        <location filename="../about_dialog.py" line="77"/>
+        <location filename="../about_dialog.py" line="92"/>
         <source>swissALTIRegio usage disclaimer</source>
         <translation>Nutzungsbedingungen swissALTIRegio</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="44"/>
+        <source>The MSS/MilX components are property of gs-soft AG</source>
+        <translation>Die MSS/MilX Komponenten sind Eigentum der gs-soft AG</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="72"/>
+        <source>Terms of use for swisstopo geodata</source>
+        <translation>Nutzungsbedingungen Geodaten swisstopo</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="83"/>
+        <source>Terms of use for Swiss GDI geodata</source>
+        <translation>Terms of use for Swiss GDI geodata</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="97"/>
+        <source>Data management and controller contact</source>
+        <translation>Kontakt Datenherr und Datenbereitstellung</translation>
+    </message>
+    <message>
+        <location filename="../about_dialog.py" line="106"/>
+        <source>Data usage liability disclaimer</source>
+        <translation>Haftungsausschluss bei Datennutzung</translation>
     </message>
 </context>
 </TS>
