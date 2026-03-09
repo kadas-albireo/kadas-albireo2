ceb76d90b2f0a1db4e57140c37dfe96c6fc650d4
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-03-09 12:24:29 +0100

    Update portfile.cmake

--- vcpkg/ports/qgis/portfile.cmake
+++ vcpkg/ports/qgis/portfile.cmake
@@ -29,8 +29,8 @@ vcpkg_from_github(
   3dfrustumfix.patch
   3dchunkloaderconcurrency.patch # https://jira.swisstopo.ch/browse/MGDIGRE_SB-1278
   mac_install_images.patch
-  flagDegreesUseUntranslatedStringSuffix.patch
-  wcsSpatialExtentSettings.patch
+  flagDegreesUseUntranslatedStringSuffix.patch # https://jira.swisstopo.ch/browse/MGDIGRE_SB-1272
+  wcsSpatialExtentSettings.patch # https://jira.swisstopo.ch/browse/MGDIGRE_SB-1201
 )
 
 file(REMOVE ${SOURCE_PATH}/cmake/FindGDAL.cmake)
