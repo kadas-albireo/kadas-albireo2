3000a43495103b0e216132b439c5975bacb7a02a
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-04-30 14:42:34 +0200

    anno-only: add Kadas-internal Circle annotation item and controller
    Introduces KadasCircleAnnotationItem (type id 'kadas:circle'), a direct
    QgsAnnotationItem subclass storing a center point and a ring point (a
    point on the circumference). Renders as a 64-segment polygon
    approximation using a QgsFillSymbol. Bounding box is derived from
    center + planar radius.
    
    KadasCircleAnnotationController provides the two-click drawing UX
    (first click sets center, second click finishes via the rubber-band
    ring point) and edit handles for both nodes plus a whole-item move.
    KML export tessellates the circle into a LinearRing in EPSG:4326.
    
    Multi-part and geodesic mode are not supported in v1; they will be
    added back as further Kadas-internal items if needed.

---
+++ kadas/gui/annotationitems/kadascircleannotationcontroller.h
@@ -0,0 +1,87 @@
+/***************************************************************************
+    kadascircleannotationcontroller.h
+    ---------------------------------
+    copyright            : (C) 2026 by Denis Rouzaud
+    email                : denis at opengis dot ch
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef KADASCIRCLEANNOTATIONCONTROLLER_H
+#define KADASCIRCLEANNOTATIONCONTROLLER_H
+
+#include "kadas/gui/annotationitems/kadasannotationitemcontroller.h"
+
+/**
+ * \ingroup gui
+ * \brief Controller for \c KadasCircleAnnotationItem (type id \c "kadas:circle").
+ *
+ * Two-click drawing UX: first click sets the center, second click sets the
+ * radius via a point on the circumference. Edit handles expose center and
+ * ring point.
+ *
+ * Multi-part and geodesic mode are not supported in v1.
+ */
+class KADAS_GUI_EXPORT KadasCircleAnnotationController : public KadasAnnotationItemController
+{
+  public:
+    KadasCircleAnnotationController() = default;
+
+    QString itemType() const override;
+    QString itemName() const override;
+    QgsAnnotationItem *createItem() const override;
+
+    QList<KadasMapItem::Node> nodes( const QgsAnnotationItem *item, const KadasAnnotationItemContext &ctx ) const override;
+
+    bool startPart( QgsAnnotationItem *item, const KadasMapPos &firstPoint, const KadasAnnotationItemContext &ctx ) override;
+    bool startPart( QgsAnnotationItem *item, const KadasMapItem::AttribValues &values, const KadasAnnotationItemContext &ctx ) override;
+    void setCurrentPoint( QgsAnnotationItem *item, const KadasMapPos &p, const KadasAnnotationItemContext &ctx ) override;
+    void setCurrentAttributes( QgsAnnotationItem *item, const KadasMapItem::AttribValues &values, const KadasAnnotationItemContext &ctx ) override;
+    bool continuePart( QgsAnnotationItem *item, const KadasAnnotationItemContext &ctx ) override;
+    void endPart( QgsAnnotationItem *item ) override;
+
+    KadasMapItem::AttribDefs drawAttribs() const override;
+    KadasMapItem::AttribValues drawAttribsFromPosition( const QgsAnnotationItem *item, const KadasMapPos &pos, const KadasAnnotationItemContext &ctx ) const override;
+    KadasMapPos positionFromDrawAttribs( const QgsAnnotationItem *item, const KadasMapItem::AttribValues &values, const KadasAnnotationItemContext &ctx ) const override;
+
+    KadasMapItem::EditContext getEditContext( const QgsAnnotationItem *item, const KadasMapPos &pos, const KadasAnnotationItemContext &ctx ) const override;
+    void edit( QgsAnnotationItem *item, const KadasMapItem::EditContext &editContext, const KadasMapPos &newPoint, const KadasAnnotationItemContext &ctx ) override;
+    void edit( QgsAnnotationItem *item, const KadasMapItem::EditContext &editContext, const KadasMapItem::AttribValues &values, const KadasAnnotationItemContext &ctx ) override;
+    KadasMapItem::AttribValues editAttribsFromPosition(
+      const QgsAnnotationItem *item, const KadasMapItem::EditContext &editContext, const KadasMapPos &pos, const KadasAnnotationItemContext &ctx
+    ) const override;
+    KadasMapPos positionFromEditAttribs(
+      const QgsAnnotationItem *item, const KadasMapItem::EditContext &editContext, const KadasMapItem::AttribValues &values, const KadasAnnotationItemContext &ctx
+    ) const override;
+
+    KadasItemPos position( const QgsAnnotationItem *item ) const override;
+    void setPosition( QgsAnnotationItem *item, const KadasItemPos &pos ) override;
+    void translate( QgsAnnotationItem *item, double dx, double dy ) override;
+
+    QString asKml( const QgsAnnotationItem *item, const QgsCoordinateReferenceSystem &itemCrs, const QgsRenderContext &renderContext, QuaZip *kmzZip = nullptr ) const override;
+
+  private:
+    enum AttribIds
+    {
+      AttrX,
+      AttrY,
+      AttrR
+    };
+
+    enum class Phase
+    {
+      Center, //!< awaiting first click (center). startPart will switch to Ring.
+      Ring,   //!< center placed; rubber band updates ring point.
+    };
+    // The phase is implicit: if center == ring point, we're in Ring phase
+    // following startPart; continuePart finalizes.
+};
+
+#endif // KADASCIRCLEANNOTATIONCONTROLLER_H
