d51f3bfb039c149ac2b400e139d9003f6c49b7b9
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-05-01 13:40:54 +0200

    Phase 6 slice 29: port identify dialog pin overlays to QgsRubberBand
    KadasMapIdentifyDialog held two transient pin overlays as KadasPinItem
    instances on the global KadasMapCanvasItemManager: the click-position
    pin and the per-result pin shown when the selected result is a point.
    Both are short-lived overlays that don't survive the dialog and don't
    need to participate in any layer.
    
    Replace both with QgsRubberBand point markers (ICON_FULL_BOX): the
    click pin in red, the result pin in blue. Drops the dependency on
    KadasPinItem / KadasSymbolItem / KadasMapCanvasItemManager from the
    identify dialog.
    
    Signed-off-by: Denis Rouzaud <denis@opengis.ch>

--- kadas/app/kadasmapidentifydialog.cpp
+++ kadas/app/kadasmapidentifydialog.cpp
@@ -35,10 +35,9 @@
 #include <qgis/qgsrasterlayer.h>
 #include <qgis/qgsrasteridentifyresult.h>
 #include <qgis/qgssettings.h>
+#include <qgis/qgsrubberband.h>
 #include <qgis/qgsvectorlayer.h>
 
-#include "kadas/gui/kadasmapcanvasitemmanager.h"
-#include "kadas/gui/mapitems/kadassymbolitem.h"
 #include "kadasmapidentifydialog.h"
 
 
@@ -98,10 +97,10 @@ void KadasMapIdentifyDialog::clear()
 {
   delete mRubberband;
   mRubberband = nullptr;
-  delete mResultPin.data();
-  mResultPin.clear();
-  delete mClickPosPin.data();
-  mClickPosPin.clear();
+  delete mResultPin;
+  mResultPin = nullptr;
+  delete mClickPosPin;
+  mClickPosPin = nullptr;
   qDeleteAll( mGeometries );
   mGeometries.clear();
   if ( mRasterIdentifyReply )
@@ -119,8 +118,8 @@ void KadasMapIdentifyDialog::onItemClicked( QTreeWidgetItem *item, int /*col*/ )
 {
   delete mRubberband;
   mRubberband = nullptr;
-  delete mResultPin.data();
-  mResultPin.clear();
+  delete mResultPin;
+  mResultPin = nullptr;
 
   while ( item && item->data( 0, sGeometryRole ).isNull() )
     item = item->parent();
@@ -141,9 +140,11 @@ void KadasMapIdentifyDialog::onItemClicked( QTreeWidgetItem *item, int /*col*/ )
   if ( dynamic_cast<QgsPoint *>( geom ) )
   {
     QgsPoint *p = static_cast<QgsPoint *>( geom );
-    mResultPin = new KadasPinItem( mCanvas->mapSettings().destinationCrs() );
-    mResultPin->setPosition( KadasItemPos( p->x(), p->y() ) );
-    mResultPin->setFilePath( ":/kadas/icons/pin_blue" );
+    mResultPin = new QgsRubberBand( mCanvas, Qgis::GeometryType::Point );
+    mResultPin->setIcon( QgsRubberBand::ICON_FULL_BOX );
+    mResultPin->setColor( QColor( 0, 0, 255 ) );
+    mResultPin->setIconSize( 12 );
+    mResultPin->addPoint( QgsPointXY( p->x(), p->y() ) );
   }
   else
   {
@@ -160,9 +161,11 @@ void KadasMapIdentifyDialog::collectInfo( const QgsPointXY &mapPos )
 {
   clear();
 
-  mClickPosPin = new KadasPinItem( mCanvas->mapSettings().destinationCrs() );
-  mClickPosPin->setPosition( KadasItemPos( mapPos.x(), mapPos.y() ) );
-  KadasMapCanvasItemManager::addItem( mClickPosPin );
+  mClickPosPin = new QgsRubberBand( mCanvas, Qgis::GeometryType::Point );
+  mClickPosPin->setIcon( QgsRubberBand::ICON_FULL_BOX );
+  mClickPosPin->setColor( QColor( 255, 0, 0 ) );
+  mClickPosPin->setIconSize( 12 );
+  mClickPosPin->addPoint( mapPos );
 
   // Prepare for raster layers
   const QgsCoordinateReferenceSystem &mapCrs = mCanvas->mapSettings().destinationCrs();
--- kadas/app/kadasmapidentifydialog.h
+++ kadas/app/kadasmapidentifydialog.h
@@ -29,11 +29,10 @@ class QgsAbstractGeometry;
 class QgsFeature;
 class QgsGeometryRubberBand;
 class QgsMapCanvas;
-class QgsPinAnnotationItem;
 class QgsRasterIdentifyResult;
 class QgsRasterLayer;
+class QgsRubberBand;
 class QgsVectorLayer;
-class KadasPinItem;
 
 
 class KadasMapIdentifyDialog : public QDialog
@@ -54,8 +53,8 @@ class KadasMapIdentifyDialog : public QDialog
     QgsMapCanvas *mCanvas = nullptr;
     QTreeWidget *mTreeWidget = nullptr;
     QgsGeometryRubberBand *mRubberband = nullptr;
-    QPointer<KadasPinItem> mClickPosPin;
-    QPointer<KadasPinItem> mResultPin;
+    QgsRubberBand *mClickPosPin = nullptr;
+    QgsRubberBand *mResultPin = nullptr;
     QList<QgsAbstractGeometry *> mGeometries;
     QTimer *mTimeoutTimer = nullptr;
     QNetworkReply *mRasterIdentifyReply = nullptr;
