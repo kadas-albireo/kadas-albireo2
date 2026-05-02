15642dc4591023f78b891146d954cac75225bea5
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-05-02 22:16:18 +0200

    Phase 6 slice 71: Drop stale Python bindings for deleted MilX classes
    `scripts/sipify_all.sh` regenerates `kadasgui_auto.sip` from the
    list of headers in kadas/gui, but it does not prune
    `auto_generated/**/*.sip.in` or `auto_additions/*.py` for
    classes whose headers have been removed. After slices 51 and 68
    deleted KadasMilxEditor / KadasMilxLayer / KadasMilxItem, their
    generated bindings were still being %Include'd, breaking
    `make python_module_kadas__kadasgui`.
    
    - Delete the six orphan files:
      - python/kadasgui/auto_generated/milx/kadasmilx{editor,item,layer}.sip.in
      - python/kadasgui/auto_additions/kadasmilx{editor,item,layer}.py
    - Drop the matching `%Include` lines from kadasgui_auto.sip.
    - Refresh the still-live bindings whose signatures changed:
      - kadasmilxlayerpropertiespage.sip.in: ctor takes
        `QgsMapLayer *` (slice 48), not `KadasMilxLayer *`.
      - kadasitemlayer.sip.in: drop the protected
        `KadasItemLayer(name, crs, layerType)` ctor (slice 69).
    
    Full build with python_module_kadas__kadasgui green.

--- python/kadasgui/auto_additions/kadasmilxeditor.py
+++
@@ -1,5 +0,0 @@
-# The following has been generated automatically from kadas/gui/milx/kadasmilxeditor.h
-try:
-    KadasMilxEditor.__overridden_methods__ = ['syncItemToWidget', 'syncWidgetToItem']
-except (NameError, AttributeError):
-    pass
--- python/kadasgui/auto_additions/kadasmilxitem.py
+++
@@ -1,9 +0,0 @@
-# The following has been generated automatically from kadas/gui/milx/kadasmilxitem.h
-try:
-    KadasMilxItem.fromMilx = staticmethod(KadasMilxItem.fromMilx)
-    KadasMilxItem.fromMssStringAndPoints = staticmethod(KadasMilxItem.fromMssStringAndPoints)
-    KadasMilxItem.computeScreenExtent = staticmethod(KadasMilxItem.computeScreenExtent)
-    KadasMilxItem.validateMssString = staticmethod(KadasMilxItem.validateMssString)
-    KadasMilxItem.__overridden_methods__ = ['itemName', 'symbolImage', 'symbolAnchor', 'boundingBox', 'margin', 'nodes', 'intersects', 'hitTest', 'closestPoint', 'render', 'assign', 'clone', 'serialize', 'deserialize', 'setState', 'startPart', 'setCurrentPoint', 'setCurrentAttributes', 'continuePart', 'endPart', 'drawAttribs', 'drawAttribsFromPosition', 'positionFromDrawAttribs', 'getEditContext', 'edit', 'populateContextMenu', 'onDoubleClick', 'editAttribsFromPosition', 'positionFromEditAttribs', 'position', 'setPosition', '_clone', 'createEmptyState']
-except (NameError, AttributeError):
-    pass
