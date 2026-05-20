726f8874aee44152ae9f5df3d3076faafae43475
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-05-20 13:18:20 +0200

    Align shadow/skybox settings widgets with bumped QGIS API
    QgsShadowSettings::shadowMapResolution()/setShadowMapResolution() and QgsSkyboxSettings::skyboxType()/setSkyboxType()/setPanoramicTexturePath() (along with the QgsSkyboxEntity::SkyboxType enum) were removed upstream. Mirror QGIS's own qgs(shadow|skybox)renderingsettingswidget.cpp from the pinned commit: drop the shadow map resolution control and switch the skybox widget to Qgis::Map3DBackgroundType plus the new Qgis::SkyboxCubeMapping selector.

--- kadas/app/3d/kadasskyboxrenderingsettingswidget.cpp
+++ kadas/app/3d/kadasskyboxrenderingsettingswidget.cpp
@@ -17,35 +17,39 @@
 
 #include <QCheckBox>
 #include <QLineEdit>
+#include <QString>
 #include "qgs3dmapsettings.h"
 #include "qgis.h"
+
+// this is broken for z-up coordinate system
+#define ENABLE_PANORAMIC_SKYBOX 0
 
 KadasSkyboxRenderingSettingsWidget::KadasSkyboxRenderingSettingsWidget( QWidget *parent )
   : QWidget( parent )
 {
   setupUi( this );
-
-  // To future maintainers: make sure the order of added items is the same as the order at QgsSkyboxEntity::SkyboxType
-  skyboxTypeComboBox->addItem( tr( "Panoramic Texture" ) );
-  skyboxTypeComboBox->addItem( tr( "Distinct Faces" ) );
+#if ENABLE_PANORAMIC_SKYBOX
+  skyboxTypeComboBox->addItem( tr( "Panoramic Texture" ), QVariant::fromValue( Qgis::Map3DBackgroundType::NoBackground ) ); // this will have to be fixed when panoramic skybox is fixed
+#endif
+  skyboxTypeComboBox->addItem( tr( "Distinct Faces" ), QVariant::fromValue( Qgis::Map3DBackgroundType::DistinctTextureSkybox ) );
   connect( skyboxTypeComboBox, qOverload<int>( &QComboBox::currentIndexChanged ), this, &KadasSkyboxRenderingSettingsWidget::showSkyboxSettings );
+
+  mMappingComboBox->addItem( tr( "Native (Z-Up)" ), QVariant::fromValue( Qgis::SkyboxCubeMapping::NativeZUp ) );
+  mMappingComboBox->addItem( tr( "OpenGL / WebGL (Y-Up)" ), QVariant::fromValue( Qgis::SkyboxCubeMapping::OpenGLYUp ) );
+  mMappingComboBox->addItem( tr( "Godot Engine (Y-Up)" ), QVariant::fromValue( Qgis::SkyboxCubeMapping::GodotYUp ) );
+  mMappingComboBox->addItem( tr( "Unreal Engine (Z-Up)" ), QVariant::fromValue( Qgis::SkyboxCubeMapping::UnrealEngineZUp ) );
+  mMappingComboBox->addItem( tr( "Unity Engine / Left-Handed (Y-Up)" ), QVariant::fromValue( Qgis::SkyboxCubeMapping::LeftHandedYUpMirrored ) );
 
   showSkyboxSettings( 0 );
 }
 
 void KadasSkyboxRenderingSettingsWidget::setSkyboxSettings( const QgsSkyboxSettings &skyboxSettings )
 {
-  switch ( skyboxSettings.skyboxType() )
-  {
-    case QgsSkyboxEntity::PanoramicSkybox:
-      skyboxTypeComboBox->setCurrentIndex( 0 ); // "Panoramic Texture"
-      break;
-    case QgsSkyboxEntity::DistinctTexturesSkybox:
-      skyboxTypeComboBox->setCurrentIndex( 1 ); // "Distinct Faces"
-      break;
-  }
+  skyboxTypeComboBox->setCurrentIndex( skyboxTypeComboBox->findData( QVariant::fromValue( skyboxSettings.type() ) ) );
 
+#if ENABLE_PANORAMIC_SKYBOX
   panoramicTextureImageSource->setSource( skyboxSettings.panoramicTexturePath() );
+#endif
   QMap<QString, QString> cubeMapFaces = skyboxSettings.cubeMapFacesPaths();
   posXImageSource->setSource( cubeMapFaces[QStringLiteral( "posX" )] );
   posYImageSource->setSource( cubeMapFaces[QStringLiteral( "posY" )] );
@@ -53,41 +57,47 @@ void KadasSkyboxRenderingSettingsWidget::setSkyboxSettings( const QgsSkyboxSetti
   negXImageSource->setSource( cubeMapFaces[QStringLiteral( "negX" )] );
   negYImageSource->setSource( cubeMapFaces[QStringLiteral( "negY" )] );
   negZImageSource->setSource( cubeMapFaces[QStringLiteral( "negZ" )] );
+
+  mMappingComboBox->setCurrentIndex( mMappingComboBox->findData( QVariant::fromValue( skyboxSettings.cubeMapping() ) ) );
 }
 
 QgsSkyboxSettings KadasSkyboxRenderingSettingsWidget::toSkyboxSettings()
 {
   QgsSkyboxSettings settings;
-  settings.setSkyboxType( static_cast<QgsSkyboxEntity::SkyboxType>( skyboxTypeComboBox->currentIndex() ) );
+#if ENABLE_PANORAMIC_SKYBOX
   settings.setPanoramicTexturePath( panoramicTextureImageSource->source() );
+#endif
   settings.setCubeMapFace( QStringLiteral( "posX" ), posXImageSource->source() );
   settings.setCubeMapFace( QStringLiteral( "posY" ), posYImageSource->source() );
   settings.setCubeMapFace( QStringLiteral( "posZ" ), posZImageSource->source() );
   settings.setCubeMapFace( QStringLiteral( "negX" ), negXImageSource->source() );
   settings.setCubeMapFace( QStringLiteral( "negY" ), negYImageSource->source() );
   settings.setCubeMapFace( QStringLiteral( "negZ" ), negZImageSource->source() );
+  settings.setCubeMapping( mMappingComboBox->currentData().value<Qgis::SkyboxCubeMapping>() );
   return settings;
 }
 
 void KadasSkyboxRenderingSettingsWidget::showSkyboxSettings( int )
 {
-  const QgsSkyboxEntity::SkyboxType type = static_cast<QgsSkyboxEntity::SkyboxType>( skyboxTypeComboBox->currentIndex() );
-  const bool isPanoramic = type == QgsSkyboxEntity::PanoramicSkybox;
-  const bool isDistinctFaces = type == QgsSkyboxEntity::DistinctTexturesSkybox;
+  const Qgis::Map3DBackgroundType type = skyboxTypeComboBox->currentData().value<Qgis::Map3DBackgroundType>();
+  bool showPanoramicWidgets = false;
+  bool showDistinctFacesWidgets = false;
 
-  panoramicTextureLabel->setVisible( isPanoramic );
-  panoramicTextureImageSource->setVisible( isPanoramic );
+  switch ( type )
+  {
+#if ENABLE_PANORAMIC_SKYBOX
+    case Qgis::Map3DBackgroundType::NoBackground: // this needs fixing when panoramic skybox is fixed
+      showPanoramicWidgets = true;
+      break;
+#endif
+    case Qgis::Map3DBackgroundType::DistinctTextureSkybox:
+      showDistinctFacesWidgets = true;
+      break;
+    case Qgis::Map3DBackgroundType::NoBackground:
+    case Qgis::Map3DBackgroundType::FixedGradientBackground:
+      break;
+  }
 
-  negXImageSourceLabel->setVisible( isDistinctFaces );
-  negXImageSource->setVisible( isDistinctFaces );
-  negYImageSourceLabel->setVisible( isDistinctFaces );
-  negYImageSource->setVisible( isDistinctFaces );
-  negZImageSourceLabel->setVisible( isDistinctFaces );
-  negZImageSource->setVisible( isDistinctFaces );
-  posXImageSourceLabel->setVisible( isDistinctFaces );
-  posXImageSource->setVisible( isDistinctFaces );
-  posYImageSourceLabel->setVisible( isDistinctFaces );
-  posYImageSource->setVisible( isDistinctFaces );
-  posZImageSourceLabel->setVisible( isDistinctFaces );
-  posZImageSource->setVisible( isDistinctFaces );
+  mPanoramicWidget->setVisible( showPanoramicWidgets );
+  mCubeMapWidget->setVisible( showDistinctFacesWidgets );
 }
