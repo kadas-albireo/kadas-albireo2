fd157e0508cb02335abdc44746270d922bfa4261
Damiano Lombardi <damiano@opengis.ch>
2026-06-03 12:27:17 +0200

    Arcgis rest layer symbology support rotation variables

--- vcpkg/ports/qgis/portfile.cmake
+++ vcpkg/ports/qgis/portfile.cmake
@@ -32,6 +32,7 @@ vcpkg_from_github(
   flagDegreesUseUntranslatedStringSuffix.patch # https://jira.swisstopo.ch/browse/MGDIGRE_SB-1272
   wcsSpatialExtentSettings.patch # https://jira.swisstopo.ch/browse/MGDIGRE_SB-1201
   bigobj-vectorlayer.patch # https://github.com/qgis/QGIS/pull/66271
+  arcgisrestSymbolsRotation.patch
 )
 
 file(REMOVE ${SOURCE_PATH}/cmake/FindGDAL.cmake)
