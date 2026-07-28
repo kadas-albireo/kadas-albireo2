7355bef7925b17734a655f055395d94dfb490c48
Valentin Buira <30632058+ValentinBuira@users.noreply.github.com>
2026-07-28 12:41:43 +0200

    New (un)lock layer entry in layer tree menu (#476)
    * Add a new entry to layer tree menu to lock and unlock a layer aka exposing the required and read only value in Kadas

---
+++ kadas/app/external/qgis/app/qgslayertreeviewnonremovableindicator.cpp
@@ -0,0 +1,60 @@
+/***************************************************************************
+  qgslayertreeviewnonremovableindicator.cpp
+  --------------------------------------
+  Date                 : Sep 2018
+  Copyright            : (C) 2018 by Martin Dobias
+  Email                : wonder dot sk at gmail dot com
+ ***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qgslayertreeviewnonremovableindicator.h"
+
+#include "qgslayertree.h"
+#include "qgslayertreemodel.h"
+#include "qgslayertreeutils.h"
+#include "qgslayertreeview.h"
+
+#include <QString>
+
+#include "moc_qgslayertreeviewnonremovableindicator.cpp"
+
+using namespace Qt::StringLiterals;
+
+QgsLayerTreeViewNonRemovableIndicatorProvider::QgsLayerTreeViewNonRemovableIndicatorProvider( QgsLayerTreeView *view )
+  : QgsLayerTreeViewIndicatorProvider( view )
+{}
+
+QString QgsLayerTreeViewNonRemovableIndicatorProvider::iconName( QgsMapLayer *layer )
+{
+  Q_UNUSED( layer )
+  return u"/mIndicatorNonRemovable.svg"_s;
+}
+
+QString QgsLayerTreeViewNonRemovableIndicatorProvider::tooltipText( QgsMapLayer *layer )
+{
+  Q_UNUSED( layer )
+  return tr( "Layer required by the project" );
+}
+
+bool QgsLayerTreeViewNonRemovableIndicatorProvider::acceptLayer( QgsMapLayer *layer )
+{
+  return !layer->flags().testFlag( QgsMapLayer::LayerFlag::Removable );
+}
+
+void QgsLayerTreeViewNonRemovableIndicatorProvider::connectSignals( QgsMapLayer *layer )
+{
+  QgsLayerTreeViewIndicatorProvider::connectSignals( layer );
+  connect( layer, &QgsMapLayer::flagsChanged, this, &QgsLayerTreeViewNonRemovableIndicatorProvider::onLayerChanged );
+}
+
+void QgsLayerTreeViewNonRemovableIndicatorProvider::disconnectSignals( QgsMapLayer *layer )
+{
+  QgsLayerTreeViewIndicatorProvider::disconnectSignals( layer );
+  disconnect( layer, &QgsMapLayer::flagsChanged, this, &QgsLayerTreeViewNonRemovableIndicatorProvider::onLayerChanged );
+}
--- kadas/app/kadasmainwindow.cpp
+++ kadas/app/kadasmainwindow.cpp
@@ -55,6 +55,8 @@
 #include <qgis/qgselevationutils.h>
 #include <qgis/qgsrasterlayerelevationproperties.h>
 
+#include "external/qgis/app/qgslayertreeviewnonremovableindicator.h"
+
 #include "kadas/core/kadas.h"
 #include "kadas/core/kadassettingstree.h"
 #include "kadas/gui/kadasclipboard.h"
@@ -354,6 +356,10 @@ void KadasMainWindow::init()
   mKadasTemporalController = new KadasTemporalController( mapCanvas() );
   mKadasTemporalController->hide();
   new KadasLayerTreeViewTemporalIndicator( mLayerTreeView, mKadasTemporalController ); // gets parented to the layer view
+
+  // Private layer tree indicator
+  new QgsLayerTreeViewNonRemovableIndicatorProvider( mLayerTreeView ); // gets parented to the layer view
+
 
   // Plugin manager
   mPluginManager = new KadasPluginManager( mapCanvas(), mActionPluginManager );
