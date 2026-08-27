0ed68051c5bc59963a3fcee979a437aa83548e2f
Damiano Lombardi <damiano@opengis.ch>
2026-08-27 20:20:43 +0200

    Use applicationName() in OAuth2 browser messages (#526)

--- vcpkg/ports/qgis/portfile.cmake
+++ vcpkg/ports/qgis/portfile.cmake
@@ -39,6 +39,7 @@ vcpkg_from_github(
   # canvas layers + raster elevation surface z-range from band statistics
   annotation-3d-picture-billboards.patch # https://github.com/qgis/QGIS/pull/67135
   # picture annotation items as 3d billboards
+  oauthApplicationName.patch
 )
 
 file(REMOVE ${SOURCE_PATH}/cmake/FindGDAL.cmake)
