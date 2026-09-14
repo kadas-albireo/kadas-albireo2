7d7a9bc4b0d2174d96d359f852327e959e6731c6
Denis Rouzaud <denis.rouzaud@gmail.com>
2026-09-14 20:16:12 +0200

    fix groups

--- python/kadasgui/auto_additions/kadasmilxlibrary.py
+++ python/kadasgui/auto_additions/kadasmilxlibrary.py
@@ -1,5 +1,6 @@
 # The following has been generated automatically from kadas/gui/milx/kadasmilxlibrary.h
 try:
+    KadasMilxLibrary.localizedName = staticmethod(KadasMilxLibrary.localizedName)
     KadasMilxLibrary.__signal_arguments__ = {'symbolSelected': ['symbolDesc: KadasMilxSymbolDesc'], 'visibilityChanged': ['visible: bool']}
 except (NameError, AttributeError):
     pass
